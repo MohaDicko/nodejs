d0965625bf14405ce5fcece5076517364533e77b
yelmouss <yelmouss.devt@gmail.com>
2024-05-20 13:50:50 +0100

    first commit

---
+++ email-sender.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'your-generated-password'
+  }
+});
+
+var mailOptions = {
+  from: 'YELMOUSS DEVT',
+  to: 'mouna.bensalah111@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
